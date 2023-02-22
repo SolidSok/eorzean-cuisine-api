37136486bec21a6f9e8dfd7c49c794811a22c2b1
SolidSok <solidsok@gmail.com>
2023-02-21 21:23:27 -0800

    Update index.js

--- index.js
+++ index.js
@@ -29,21 +29,27 @@ let allowedOrigins = [
   'http://localhost:8080',
   'http://localhost:1234',
   'http://localhost:3000',
-  'https://eorzean-cuisine-client.herokuapp.com/menu',
+  'https://eorzean-cuisine-client.herokuapp.com',
 ];
 
+// app.use(
+//   cors({
+//     origin: (origin, callback) => {
+//       if (!origin) return callback(null, true);
+//       if (allowedOrigins.indexOf(origin) === -1) {
+//         let message =
+//           'The CORS policy for this application doesn’t allow access from origin ' +
+//           origin;
+//         return callback(new Error(message), false);
+//       }
+//       return callback(null, true);
+//     },
+//   })
+// );
 app.use(
   cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        let message =
-          'The CORS policy for this application doesn’t allow access from origin ' +
-          origin;
-        return callback(new Error(message), false);
-      }
-      return callback(null, true);
-    },
+    origin: 'https://eorzean-cuisine-client.herokuapp.com',
+    methods: ['GET'],
   })
 );
 
