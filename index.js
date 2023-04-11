9bc8f821f58f8955571ad25dec704d8299ea0e2c
SolidSok <solidsok@gmail.com>
2023-04-10 22:07:44 -0700

    Update index.js

--- index.js
+++ index.js
@@ -29,29 +29,29 @@ let allowedOrigins = [
   'http://localhost:8080',
   'http://localhost:1234',
   'http://localhost:3000',
-  'https://eorzean-cuisine-client.herokuapp.com',
+  'https://eorzean-cuisine.herokuapp.com',
 ];
 
-// app.use(
-//   cors({
-//     origin: (origin, callback) => {
-//       if (!origin) return callback(null, true);
-//       if (allowedOrigins.indexOf(origin) === -1) {
-//         let message =
-//           'The CORS policy for this application doesn’t allow access from origin ' +
-//           origin;
-//         return callback(new Error(message), false);
-//       }
-//       return callback(null, true);
-//     },
-//   })
-// );
 app.use(
   cors({
-    origin: 'https://eorzean-cuisine.herokuapp.com/',
-    methods: ['GET'],
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        let message =
+          'The CORS policy for this application doesn’t allow access from origin ' +
+          origin;
+        return callback(new Error(message), false);
+      }
+      return callback(null, true);
+    },
   })
 );
+// app.use(
+//   cors({
+//     origin: 'https://eorzean-cuisine.herokuapp.com/',
+//     methods: ['GET'],
+//   })
+// );
 
 let auth = require('./auth')(app);
 const passport = require('passport');
